f80870735db131f863273070fdbf9b078c615518
Shankhadeep1234 <shankhadeepraj@gmail.com>
2020-04-11 16:00:05 +0530

    initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav class="main-nav">
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link :to="{ name: 'About' }">About</router-link></li>
+    </ul>
+    <h2>User Profiles</h2>
+    <ul>
+      <li v-for="(id, index) in userIds" :key="index">
+        <router-link :to="{ name: 'ViewProfile', params: { user_id: id } }"
+          ><span> Profile : {{ id }} </span></router-link
+        >
+      </li>
+    </ul>
+    <h2>Navigation Control</h2>
+    <ul>
+      <li><button @click="goBack">Go Back</button></li>
+      <li><button @click="goHome">Redirect to Home</button></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      userIds: ["1", "2", "3", "4"]
+    };
+  },
+  methods: {
+    goHome() {
+      this.$router.push({ name: "Home" });
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+a {
+  color: #42b983;
+}
+</style>
